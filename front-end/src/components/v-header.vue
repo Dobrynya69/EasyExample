e94dce2c11cdd80504f15333d0591d74a8b6aa91
Mykyta <myk23ta@gmail.com>
2023-04-26 13:50:54 +0300

    need to fix

--- front-end/src/components/v-header.vue
+++ front-end/src/components/v-header.vue
@@ -32,6 +32,7 @@ export default{
     width: 100%;
     height: 80px;
     position: fixed;
+    z-index: 100;
     top: 0;
     left: 0;
 }
--- front-end/src/views/Thingsview.vue
+++ front-end/src/views/Thingsview.vue
@@ -9,9 +9,9 @@ export default{
         const  url = computed(() => {
             return `http://127.0.0.1:8000/api/thing?string=&page=${page.value}`
         });
-        const { data } = await useFetch(url, {refetch: true}).json();
+        const { isFetching, data } = await useFetch(url, {refetch: true}).json();
         return{
-            data,
+            data, isFetching
         }
     },
     methods:{
@@ -25,41 +25,114 @@ export default{
         }
     }
 }
-
 </script>
 <template>
     <div class="container">
-        <div class="container__row" >
-            <div class="container__item" v-for="da in data.results">
-                <img :src="da.image" class="item__img" alt="" />
-                <div class="item__title">{{ da.title }}</div>
-                <div class="item__year">{{ da.year }}</div>
+        <div class="filter__menu">
+            <div class="menu__title"><div>Filter</div></div>
+            <div class="menu__content"></div>
+        </div>
+        <div class="container__items">
+            <div class="items__title"><div class="item__text">List</div></div>
+                <div class="container__row">
+                    <div class="container__item" v-for="da in data.results">
+                        <img :src="da.image" class="item__img" alt="" />
+                        <div class="item__title"><div class="title__text">{{ da.title }}</div>
+                    </div>
+                </div>
             </div>
         </div>
-        <button class="ff" @click="previousPage()">ЖМИ ЕСЛИ НЕ МУЖИК</button>
-        <button class="ff" @click="nextPage()">ЖМИ</button>
     </div>
 </template>
+        <!-- <button class="ff" @click="previousPage()">ЖМИ ЕСЛИ НЕ МУЖИК</button>
+        <button class="ff" @click="nextPage()">ЖМИ</button> -->
 <style>
+.filter__menu {
+    min-width: 300px;
+    height: 100vh;
+    position: relative;
+}
+.menu__title div{
+    font-family: 'Encode Sans SC', sans-serif;
+}
+.menu__title {
+
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #a1a1a17c;
+    color: #fff;
+    font-size: 38px;
+    padding: 10px 0px;
+    position: absolute;
+    left: 0;
+    top: 0;
+}
+.menu__content {
+    height: 100%;
+    background-color: #545454;
+}
+.container{
+    display: flex;
+    
+}
+.items__title{
+    width: 100%;
+    background-color: #A1A1A1;
+    padding: 10px 0px;
+    font-size: 38px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.items__title div{
+    font-family: 'Encode Sans SC', sans-serif;
+}
+.container__items{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
 .container__row{
     display: flex;
-    justify-content: space-between;
     flex-wrap: wrap;
-    padding: 10px 0px;
+
+    margin: 10px auto;
+    justify-content: start;
+    
 }
 .container__item{
-    background-color: #0e0e0e;
-    color: #fff;
-    width: 300px;
-    text-align: center;
-    padding: 10px;
-    margin-bottom: 10px;
-    font-size: 20px;
+    width: 250px;
+    height: 300px;
+    position: relative;
+    margin: 10px 50px;
+
 }
-.item__img{
-    height: 400px;
-    max-width: 100%;
+.item__img {
+    width: 100%;
+    height: 100%;
     object-fit: cover;
 }
+.item__title {
+    width: calc(100% - 20px);
+    min-height: 50px;
+    padding: 10px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 10;
+    background-color: #000000b0;
+    color: #fff;
+    display: flex;
+    align-items: center;
+}
+.title__text{
+    text-align: center;
+    width: 100%;
+    height: 100%;
+}
+
 </style>
 
