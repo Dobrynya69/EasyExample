9e8651a3042c69d674795ace79d755bc90c7814b
Roman <roma040805@gmail.com>
2023-05-03 22:56:52 +0300

    Add anime detail test

--- api/tests/test_anime_api.py
+++ api/tests/test_anime_api.py
@@ -1,24 +1,8 @@
-from django.urls import path, reverse, include, resolve
+from django.urls import reverse, resolve
 from django.test import SimpleTestCase
 from api.views import *
 from api.models import *
-from rest_framework.test import APITestCase, APIClient
-from rest_framework.authtoken.models import Token
-from rest_framework import status
-from django.contrib.auth.models import User
-from rest_framework.views import APIView
-
-
-class ApiUrlsTests(SimpleTestCase):
-
-    def test_anime_list_url(self):
-        url = reverse('anime')
-        self.assertEquals(resolve(url).func.view_class, AnimeViewSet)
-
-
-    def test_genre_list_url(self):
-        url = reverse('genre')
-        self.assertEquals(resolve(url).func.view_class, GenreView)
+from rest_framework.test import APITestCase
 
 
 class AnimeListAPIViewTests(APITestCase):
@@ -32,11 +16,39 @@ class AnimeListAPIViewTests(APITestCase):
         self.model_genre_class.objects.create(name='genre2')
         self.model_genre_class.objects.create(name='genre3')
 
-        self.model_class.objects.create(title='title1', image='image1.jpg', genres={'genre1', 'genre2'}, description='description1')
-        self.model_class.objects.create(title='title2', image='image2.jpg', genres={'genre2', 'genre1'}, description='description1')
-        self.model_class.objects.create(title='title3', image='image3.jpg', genres={'genre3', 'genre1'}, description='description1')
+        anime1 = self.model_class.objects.create(title='title1', image='image1.jpg', description='description1')
+        anime1.genres.set(({'genre1', 'genre2'}))
+        anime1.save()
+
+        anime2 = self.model_class.objects.create(title='title2', image='image2.jpg', description='description1')
+        anime2.genres.set(({'genre2', 'genre1'}))
+        anime2.save()
+
+        anime3 = self.model_class.objects.create(title='title3', image='image3.jpg', description='description1')
+        anime3.genres.set(({'genre3', 'genre1'}))
+        anime3.save()
 
 
     def test_get_anime_list(self):
         response = self.client.get(self.anime_list_url)
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+
+class AnimeDetailAPIViewTests(APITestCase):
+    anime_detail_url = reverse('anime_detail', kwargs={"pk": "1"})
+    model_class = Anime
+    model_genre_class = Genre
+
+
+    def setUp(self):
+        self.model_genre_class.objects.create(name='genre1')
+        self.model_genre_class.objects.create(name='genre2')
+
+        anime1 = self.model_class.objects.create(title='title1', image='image1.jpg', description='description1')
+        anime1.genres.set(({'genre1', 'genre2'}))
+        anime1.save()
+
+
+    def test_get_anime_detail(self):
+        response = self.client.get(self.anime_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
